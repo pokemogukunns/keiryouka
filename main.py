18859b713b39d6691c5b6be384cd6e1c1839cf32
pokemogukunns <nattyannkunndesu@icloud.com>
2024-09-21 09:47:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -174,6 +174,8 @@ from typing import Union
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.mount("/css", StaticFiles(directory="./css"), name="static")
 app.mount("/blog", StaticFiles(directory="./blog", html=True), name="static")
+app.mount("/home", StaticFiles(directory="./energyno", html=True), name="static")
+app.mount("/pass", StaticFiles(directory="./pass", html=True), name="static")
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 from fastapi.templating import Jinja2Templates
@@ -188,7 +190,7 @@ def home(response: Response,request: Request,yuki: Union[str] = Cookie(None)):
     if check_cokie(yuki):
         response.set_cookie("yuki","True",max_age=60 * 60 * 24 * 7)
         return template("home.html",{"request": request})
-    return redirect("/blog")
+    return redirect("/home")
 
 @app.get('/watch', response_class=HTMLResponse)
 def video(v:str,response: Response,request: Request,yuki: Union[str] = Cookie(None),proxy: Union[str] = Cookie(None)):
